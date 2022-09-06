4580fe6930e05d4ff597abf9d6185b137dfb1f2d
Albert B <abduarte20@gmail.com>
2022-09-06 01:44:27 -0700

    PuttingItTogether 1st commit

--- React/Assignments/PropItUp/prop-it-up/src/components/PersonCard.js
+++ React/Assignments/PropItUp/prop-it-up/src/components/PersonCard.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
     
 const PersonCard = (props) => {
+    const [currentAge, setAddAge] = useState(props.age);
+
     return (
         <div>
             <h1>{props.lastName}, {props.firstName}</h1>
-            <h3>Age: {props.age}</h3>
+            <h3>Age: {currentAge}</h3>
             <h3>Hair Color: {props.hairColor}</h3>
+            <button onClick={(event) => setAddAge(currentAge + 1)}>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     );
 }
