aae6bca392dd009468745ee7411b993694ac845d
Albert B <abduarte20@gmail.com>
2022-10-02 23:04:22 -0700

    app_practice_1 1st commit

---
+++ MongoDB/practice/AppPractice1/server/controllers/user.controller.js
@@ -0,0 +1,11 @@
+const User = require('../models/user.model')
+
+module.exports.findAllUsers = (req, res) => {
+    User.find()
+    .then((allUsers) => {
+        res.json({users: allUsers})
+    })
+    .catch((err) => {
+        res.json({message: 'Something went wrong', error: err})
+    })
+}
